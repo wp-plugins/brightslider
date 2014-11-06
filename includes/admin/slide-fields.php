418626c231cd3ad05e3728cbd62bc42d2181f723
themebright <themebright@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-06 15:13:20 +0000

    first commit
    git-svn-id: https://plugins.svn.wordpress.org/brightslider/trunk@1020992 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/admin/slide-fields.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Slide Fields
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Adds slide settings meta box.
+ */
+function brightslider_add_meta_box_slide_settings() {
+
+  $args = array(
+    'id'        => 'bs_slide_settings',
+    'title'     => __( 'Slide Settings', 'brightslider' ),
+    'post_type' => 'bs_slide',
+    'context'   => 'normal',
+    'priority'  => 'high',
+    'fields'    => array(
+      '_bs_show_title' => array(
+        'name'           => __( 'Show title?', 'brightslider' ),
+        'type'           => 'checkbox',
+        'checkbox_label' => __( 'Display the title above the slide text.', 'brightslider' )
+      ),
+      '_bs_slide_text' => array(
+        'name' => __( 'Slide Text', 'brightslider' ),
+        'type' => 'textarea'
+      ),
+      '_bs_slide_url'  => array(
+        'name' => __( 'Slide URL', 'brightslider' ),
+        'type' => 'url'
+      )
+    )
+  );
+
+  new CT_Meta_Box( $args );
+
+}
+add_action( 'admin_init', 'brightslider_add_meta_box_slide_settings' );
